33289407c227242beabe425c1d11fa814a84926b
Will Faught <willfaught@users.noreply.github.com>
2022-09-09 19:43:53 -0700

    Explain WSN in package doc

--- ebnf.go
+++ ebnf.go
@@ -1,4 +1,8 @@
 // Package ebnf represents and parses a variant of [Extended Backus-Naur Form] called [Wirth Syntax Notation].
+// Terminal identifiers must begin with a lowercase letter.
+// Non-terminal identifiers must begin with an uppercase letter.
+// The first production defines the start non-terminal identifier.
+// Terminal identifiers are assumed to be defined elsewhere.
 //
 // [Extended Backus-Naur Form]: https://en.wikipedia.org/wiki/Extended_Backus–Naur_form
 // [Wirth Syntax Notation]: https://en.wikipedia.org/wiki/Wirth_syntax_notation
@@ -126,7 +130,6 @@ type Factor struct {
 }
 
 // Grammar is an abstract syntax tree for a grammar.
-// The first production defines the start identifier.
 type Grammar struct {
 	Productions []*Production
 }
