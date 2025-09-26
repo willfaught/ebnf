457ade55a562da5aa19b80967073d86d58a11525
Will Faught <willfaught@users.noreply.github.com>
2025-09-25 18:06:10 -0700

    Move parser to parser.go

--- ebnf.go
+++ ebnf.go
@@ -25,7 +25,6 @@ package ebnf
 import (
 	"bytes"
 	"fmt"
-	"io"
 	"strings"
 	"unicode"
 	"unicode/utf8"
@@ -608,107 +607,4 @@ func (t Term) String() string {
 		ss[i] = fmt.Sprint(f)
 	}
 	return strings.Join(ss, " ")
-}
-
-type parser struct {
-	lexer
-	errs []error
-}
-
-func newParser(r io.RuneReader) *parser {
-	l := newLexer(r)
-	l.nextChar()
-	l.nextToken()
-	return &parser{lexer: l}
-}
-
-func (p *parser) expect(t token) {
-	if p.token == t {
-		p.nextToken()
-	} else {
-		p.errs = append(p.errs, expectedTokenError{
-			textError: textError{
-				col:  p.tokenCol,
-				line: p.tokenLine,
-			},
-			actual:   p.token,
-			expected: t,
-			text:     string(p.text),
-		})
-	}
-}
-
-func (p *parser) grammar() *Grammar {
-	if p.token != ident {
-		p.errs = append(p.errs, expectedTokenError{
-			textError: textError{
-				col:  p.tokenCol,
-				line: p.tokenLine,
-			},
-			actual:   p.token,
-			expected: ident,
-		})
-	}
-	var ps []*Production
-	for p.token == ident {
-		ps = append(ps, p.prod())
-	}
-	return &Grammar{Productions: ps}
-}
-
-func (p *parser) prod() *Production {
-	prod := &Production{Identifier: &Identifier{Text: p.text}}
-	p.nextToken()
-	p.expect(eql)
-	prod.Expression = p.expr()
-	p.expect(period)
-	return prod
-}
-
-func (p *parser) expr() *Expression {
-	ts := []*Term{p.term()}
-	for p.token == bar {
-		p.nextToken()
-		ts = append(ts, p.term())
-	}
-	return &Expression{Terms: ts}
-}
-
-func (p *parser) term() *Term {
-	fs := []*Factor{p.factor()}
-	for p.token < bar {
-		fs = append(fs, p.factor())
-	}
-	return &Term{Factors: fs}
-}
-
-func (p *parser) factor() *Factor {
-	var f Factor
-	switch p.token {
-	case ident:
-		f.Identifier = &Identifier{Text: p.text}
-		p.nextToken()
-	case literal:
-		f.Literal = &Literal{Text: p.text}
-		p.nextToken()
-	case lparen:
-		p.nextToken()
-		f.Group = p.expr()
-		p.expect(rparen)
-	case lbrak:
-		p.nextToken()
-		f.Option = p.expr()
-		p.expect(rbrak)
-	case lbrace:
-		p.nextToken()
-		f.Repetition = p.expr()
-		p.expect(rbrace)
-	default:
-		p.errs = append(p.errs, unexpectedTokenError{
-			textError: textError{col: p.tokenCol, line: p.tokenLine},
-			token:     p.token,
-			text:      p.text,
-		})
-	}
-	return &f
 }
