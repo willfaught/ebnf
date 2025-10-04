7a37101f5ede8e7fdff632cc866a436c77ae6479
Will Faught <willfaught@users.noreply.github.com>
2025-10-04 12:56:27 -0700

    Normalize phrasing in tokenString

--- tokens.go
+++ tokens.go
@@ -25,40 +25,40 @@ func tokenString(t token, text string) string {
 	switch t {
 	case tokenInvalid:
 		if text == "" {
-			s = "invalid syntax"
+			s = "invalid token"
 		} else {
-			s = fmt.Sprintf("invalid syntax %q", text)
+			s = fmt.Sprintf("invalid token %q", text)
 		}
 	case tokenIdentifier:
 		if text == "" {
-			s = "identifier"
+			s = "identifier token"
 		} else {
-			s = fmt.Sprintf("identifier %q", text)
+			s = fmt.Sprintf("identifier token %q", text)
 		}
 	case tokenLiteral:
 		if text == "" {
-			s = "literal"
+			s = "literal token"
 		} else {
-			s = fmt.Sprintf("literal %q", text)
+			s = fmt.Sprintf("literal token %q", text)
 		}
 	case tokenLeftParen:
-		s = `"("`
+		s = `token "("`
 	case tokenLeftBracket:
-		s = `"["`
+		s = `token "["`
 	case tokenLeftBrace:
-		s = `"{"`
+		s = `token "{"`
 	case tokenPipe:
-		s = `"|"`
+		s = `token "|"`
 	case tokenEqual:
-		s = `"="`
+		s = `token "="`
 	case tokenRightParen:
-		s = `")"`
+		s = `token ")"`
 	case tokenRightBracket:
-		s = `"]"`
+		s = `token "]"`
 	case tokenRightBrace:
-		s = `"}"`
+		s = `token "}"`
 	case tokenPeriod:
-		s = `"."`
+		s = `token "."`
 	case tokenEOF:
 		s = "end of file"
 	default:
